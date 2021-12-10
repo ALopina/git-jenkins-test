9619f157787a884bfd25e3e7838f100fafebe094
Aleksandra Lopina <aindifferent@gmail.com>
2021-12-10 18:22:54 +0100

    initial commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,31 @@
+package base;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resourses/chromedriver");
+
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+
+    }
+
+    public static void main(String[] args) {
+        BaseTest test = new BaseTest();
+        test.setUp();
+    }
+
+
+}
